cdbdf51fb85b32e84c6c99fcee1c69133dbedab6
Maxim Manzik <kepkpkep@gmail.com>
2024-04-10 00:14:54 +0300

    upload template inbox chat.

---
+++ app/inbox/page.tsx
@@ -0,0 +1,18 @@
+import {Mail} from "@/components/inbox/mail"
+import {mails} from "@/components/inbox/data"
+import {cookies} from "next/headers";
+
+export default function MailPage() {
+
+  const layout = cookies().get("react-resizable-panels:layout")
+  const defaultLayout = layout ? JSON.parse(layout.value) : undefined
+
+  return (
+    <div className="flex-col md:flex">
+      <Mail
+        mails={mails}
+        defaultLayout={defaultLayout}
+      />
+    </div>
+  )
+}
